4b87f6d8d945c2c5f7ea4f8f850bd52129d015af
Lubov93 <ru.lubov@gmail.com>
2021-01-29 21:54:04 +0200

    Changed settings in root directory

--- postapi/settings.py
+++ postapi/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'postapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / "db.sqlite3"),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'api',
+        'USER': 'owner1',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
