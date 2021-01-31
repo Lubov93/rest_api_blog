ec2df9832af6adef361a82b08340aa0ff8686f98
Lubov93 <ru.lubov@gmail.com>
2021-01-31 16:21:17 +0200

    Changed for static

--- postapi/settings.py
+++ postapi/settings.py
@@ -123,12 +123,11 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-TATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 
 MEDIA_URL = "/media/"
